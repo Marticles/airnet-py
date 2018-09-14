2104389654760c6e6ff219354221a922b14b7436
marticles <francis.ljh@hotmail.com>
2018-09-14 23:54:12 +0800

    完善图表

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
 from flask import Flask, render_template
 from web.get_data import get_air_data
+from flask import request
+
+import datetime
+import json
 
 app = Flask(__name__)
 
-@app.route('/airdata',methods=['GET'])
+
+@app.route('/airdata', methods=['POST', 'GET'])
 def get_data():
-    return (get_air_data())
+    if request.method == 'GET':
+        return (get_air_data())
+    elif request.method == 'POST':
+        start_time = request.get_json()['start'].replace("T", " ")
+        start_time = start_time.replace(".000Z", "")
+        start_time = datetime.datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
+        start_time += datetime.timedelta(hours=+8)
+        start_time.replace(minute=0, second=0)
+        end_time = request.get_json()['end'].replace("T", " ")
+        end_time = end_time.replace(".000Z", "")
+        end_time = datetime.datetime.strptime(end_time, "%Y-%m-%d %H:%M:%S")
+        end_time += datetime.timedelta(hours=+8)
+        end_time.replace(minute=0, second=0)
+        pollution = request.get_json()['pollution']
+        geopoint = request.get_json()['geopoint']
+        return (get_air_data(start_time, end_time, pollution,geopoint))
+
 
 @app.route('/')
 def index():
@@ -16,10 +37,30 @@ def index():
 def test():
     return render_template('test.html')
 
-@app.route('/linebarchart')
-def linechart():
 
-    return render_template('linebarchart.html')
+@app.route('/viz/line')
+def line():
+    return render_template('line.html')
+
+@app.route('/viz/bar')
+def bar():
+    return render_template('bar.html')
+
+@app.route('/viz/scatter')
+def scatter():
+    return render_template('scatter.html')
+
+@app.route('/viz/pie')
+def pie():
+    return render_template('pie.html')
+
+@app.route('/viz/rose')
+def rose():
+    return render_template('rose.html')
+
+@app.route('/viz/radar')
+def radar():
+    return render_template('radar.html')
 
 
 @app.route('/about')
--- web/get_data.py
+++ web/get_data.py
@@ -3,30 +3,86 @@ import datetime
 import json
 from lib.data_to_json import DateEncoder
 
-def get_air_data():
+
+def get_air_data(start_time='2018-04-21 01:00:00', end_time='2018-04-25 02:00:00', pollution='pm25',
+                 geopoint='yangpusipiao'):
     conn = pymysql.connect(host='localhost', port=3306, user='root', password='root', db='airnet', charset='utf8')
     cur = conn.cursor()
 
-    start_time = datetime.datetime.strptime('2018-04-25 00:01:00', "%Y-%m-%d %H:%M:%S")
-    end_time = datetime.datetime.strptime('2018-04-30 00:23:00', "%Y-%m-%d %H:%M:%S")
+    if (isinstance(start_time, str) and isinstance(end_time, str)):
+        start_time = datetime.datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
+        end_time = datetime.datetime.strptime(end_time, "%Y-%m-%d %H:%M:%S")
+    sql_default = 'SELECT time, `{0}` FROM `{1}` WHERE time BETWEEN %s AND %s'
+    sql = 'SELECT time, pm25,pm10,co,no2,ozone1hour,ozone8hour,so2,aqi,level,primarypollutant FROM `{0}` WHERE time BETWEEN %s AND %s'
 
-    sql = 'SELECT time, pm25 FROM hongkou WHERE time BETWEEN %s AND %s'
-    cur.execute(sql, (start_time, end_time))
-    data = cur.fetchall()
+    if (pollution != 'pm25'):
+        cur.execute(sql.format(geopoint), (start_time, end_time))
+        data = cur.fetchall()
+        json_data = {}
+        time = []
+        pollution = {}
+        aqi = []
+        pm25 = []
+        pm10 = []
+        co = []
+        no2 = []
+        ozone1hour = []
+        ozone8hour = []
+        so2 = []
+        level = []
+        primarypollutant = []
 
-    json_data = {}
-    time = []
-    pm25 = []
+        for single in data:
+            time.append(single[0])
+            pm25.append(single[1])
+            pm10.append(single[2])
+            co.append(single[3])
+            no2.append(single[4])
+            ozone1hour.append(single[5])
+            ozone8hour.append(single[6])
+            so2.append(single[7])
+            aqi.append(single[8])
+            level.append(single[9])
+            primarypollutant.append(single[10])
 
-    for single in data:
-        time.append(single[0])
-        pm25.append(single[1])
+        pollution['pm25'] = pm25
+        pollution['pm10'] = pm10
+        pollution['co'] = co
+        pollution['no2'] = no2
+        pollution['ozone1hour'] = ozone1hour
+        pollution['ozone8hour'] = ozone8hour
+        pollution['so2'] = so2
+        pollution['aqi'] = aqi
+        pollution['level'] = level
+        pollution['primarypollutant'] = primarypollutant
 
-    json_data['time'] = time
-    json_data['pm25'] = pm25
-    j = json.dumps(json_data, cls=DateEncoder)
+        json_data['time'] = time
+        json_data['pollution'] = pollution
+        air_json = json.dumps(json_data, cls=DateEncoder)
 
-    cur.close()
-    conn.close()
-    return j
+        cur.close()
+        conn.close()
 
+        return (air_json)
+
+    else:
+        cur.execute(sql_default.format(pollution, geopoint), (start_time, end_time))
+        data = cur.fetchall()
+        json_data = {}
+        time = []
+        pollution = []
+
+        for single in data:
+            time.append(single[0])
+            pollution.append(single[1])
+        json_data['time'] = time
+        json_data['pm25'] = pollution
+        air_json = json.dumps(json_data, cls=DateEncoder)
+        cur.close()
+        conn.close()
+        return (air_json)
+
+
+def datatime_converter(object):
+    if isinstance(object, datetime.datetime):
+        return object.__str__()
